3f085df0835c50025971cf0e5d419a1dc76c6400
googlebrain <googlebrain@163.com>
2016-09-28 16:40:06 +0800

    Initial commit

---
+++ src/com/sun/ibm/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.sun.ibm;
+
+/**
+ * Created by Administrator on 2016/9/28.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Hello World");
+    }
+}
