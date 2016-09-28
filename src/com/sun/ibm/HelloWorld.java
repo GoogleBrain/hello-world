3f6653c744744152b9bfa789379f1c6b408b9896
googlebrain <googlebrain@163.com>
2016-09-28 16:47:11 +0800

    第二次修改

--- src/com/sun/ibm/HelloWorld.java
+++ src/com/sun/ibm/HelloWorld.java
@@ -5,6 +5,7 @@ package com.sun.ibm;
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.print("Hello World");
+        System.out.println("Hello World");
+        System.out.println("我的小宝贝。。。");
     }
 }
